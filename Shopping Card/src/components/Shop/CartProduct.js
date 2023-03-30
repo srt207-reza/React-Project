db6ffa040214d30274f49567d5fca34af79a3d1c
srt207-reza <rezataghizadeh207@gmail.com>
2023-03-31 00:48:13 +0430

    Shopping Cart

---
+++ Shopping Card/src/components/Shop/CartProduct.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class CartProduct extends Component {
+
+    clickHandler (id) {
+        this.props.onRemove(id)
+    }
+
+    render() {
+
+        let {id, title, price, img} = this.props
+
+        return (
+            <div class="cart-row">
+                <div class="cart-item cart-column">
+                    <img class="cart-item-image" src={img} width="100" height="100" />
+                    <span class="cart-item-title">{title}</span>
+                </div>
+                <span class="cart-price cart-column">${price}</span>
+                <div class="cart-quantity cart-column">
+
+                    <button class="btn btn-danger" type="button" onClick={this.clickHandler.bind(this, id)}>REMOVE</button>
+                </div>
+            </div>
+        )
+    }
+}
